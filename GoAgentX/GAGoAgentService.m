627a880de939ec03cf88f846029e40088fe5f8a7
ohdarling88 <ohdarling88@gmail.com>
2012-08-26 17:50:15 +0800

    Add php fetch support

--- GoAgentX/GAGoAgentService.m
+++ GoAgentX/GAGoAgentService.m
@@ -16,9 +16,20 @@
 
 
 - (NSString *)configTemplate {
-    return [[NSString alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"goagent-config-template" ofType:@""]
+    NSString *content = [[NSString alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"goagent-config-template" ofType:@""]
                                            encoding:NSUTF8StringEncoding
                                               error:NULL];
+    BOOL usePHPFetch = [[NSUserDefaults standardUserDefaults] boolForKey:@"GoAgent:UsePHPFetch"];
+    content = [content stringByReplacingOccurrencesOfString:@"{GoAgent:UseGAEFetch}" withString:usePHPFetch ? @"0" : @"1"];
+    
+    if (usePHPFetch) {
+        // PHP 和 GAE 可能会使用同一个端口，如果使用 PHP Fetch，则先将 [listen] 中的端口设置为 0
+        NSString *localPort = [[NSUserDefaults standardUserDefaults] stringForKey:@"GoAgent:Local:Port"] ?: @"0";
+        content = [content stringByReplacingOccurrencesOfString:@"{GoAgent:Local:PaaSPort}" withString:localPort];
+        content = [content stringByReplacingOccurrencesOfString:@"{GoAgent:Local:Port}" withString:@"0"];
+    }
+    
+    return content;
 }
 
 
