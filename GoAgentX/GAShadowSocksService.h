93b4d6e1808ad9c4433df578e1a9299b21f4a5f3
messense <wapdevelop@gmail.com>
2012-11-16 20:36:33 +0800

    添加 ShadowSocks 代理支持

---
+++ GoAgentX/GAShadowSocksService.h
@@ -0,0 +1,13 @@
+//
+//  GAShadowSocksService.h
+//  GoAgentX
+//
+//  Created by messense on 12-11-16.
+//  Copyright (c) 2012年 xujiwei.com. All rights reserved.
+//
+
+#import "GAService.h"
+
+@interface GAShadowSocksService : GAService
+
+@end
