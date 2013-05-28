2e1fa401d351c468e823c2ef59c001cb5c842b29
ohdarling88 <ohdarling88@gmail.com>
2013-05-28 14:24:04 +0800

    update goagent to 3.0.1

--- GoAgentX/GAGoAgentService.m
+++ GoAgentX/GAGoAgentService.m
@@ -74,10 +74,10 @@
     NSString *pythonEggCache = [localFolder stringByAppendingPathComponent:@".python-egg-cache"];
     [[NSFileManager defaultManager] createDirectoryAtPath:pythonEggCache withIntermediateDirectories:YES attributes:nil error:NULL];
     
-    commandRunner.commandPath = @"/usr/bin/env";
-    commandRunner.arguments = [NSArray arrayWithObjects:@"python", @"proxy.py", nil];
+    commandRunner.commandPath = @"./python3/python";
+    commandRunner.arguments = [NSArray arrayWithObjects:@"proxy.py", nil];
     commandRunner.environment = [NSDictionary dictionaryWithObjectsAndKeys:
-                                 @"./:greenlet-0.4.0-py2.7-macosx-10.7-intel.egg:gevent-1.0b4-py2.7-macosx-10.7-intel.egg:greenlet-0.4.0-py2.7-macosx-10.8-intel.egg:gevent-1.0b4-py2.7-macosx-10.8-intel.egg", @"PYTHONPATH",
+//                                 @"./:greenlet-0.4.0-py2.7-macosx-10.7-intel.egg:gevent-1.0b4-py2.7-macosx-10.7-intel.egg:greenlet-0.4.0-py2.7-macosx-10.8-intel.egg:gevent-1.0b4-py2.7-macosx-10.8-intel.egg", @"PYTHONPATH",
                                  pythonEggCache,    @"PYTHON_EGG_CACHE",
                                  nil];
     commandRunner.inputText = nil;
